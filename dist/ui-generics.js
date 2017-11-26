9903c28d47ec49f6eb98c63a928f30975c3e0fa4
Jason Stiles <neosyler@gmail.com>
2017-11-26 20:54:46 +0000

    Updated build

--- dist/ui-generics.js
+++ dist/ui-generics.js
@@ -1081,6 +1081,42 @@ $templateCache.put("src/components/gen-image/templates/bind-once/linked-image.ht
     .factory('uigRestService', RestService);
 
 })();
+(function () {
+    'use strict';
+
+    function genAttr() {
+        return {
+            restrict: 'A',
+            link: function ($scope, $element, $attrs) {
+                var attr = $scope.$eval($attrs.genAttr),
+                    /**
+                     * Set attributes on element
+                     *
+                     * @param attrs
+                     */
+                    setAttrs = function (attrs) {
+                        _.forIn(attrs, function (v, k) {
+                            $element.attr(k, v);
+                        });
+                    };
+
+                $element.removeAttr('gen-attr');
+
+                if (attr) {
+                    setAttrs(attr);
+                }
+
+                $scope.$watch($attrs.genAttr, function (newVal, oldVal) {
+                    if (newVal !== oldVal) {
+                        setAttrs(newVal);
+                    }
+                });
+            }
+        };
+    }
+
+    angular.module('ui-generics').directive('genAttr', genAttr);
+})();
 /*globals _*/
 /*eslint angular/module-getter: 0*/
 (function () {
@@ -1152,42 +1188,6 @@ $templateCache.put("src/components/gen-image/templates/bind-once/linked-image.ht
 
 })();
 
-(function () {
-    'use strict';
-
-    function genAttr() {
-        return {
-            restrict: 'A',
-            link: function ($scope, $element, $attrs) {
-                var attr = $scope.$eval($attrs.genAttr),
-                    /**
-                     * Set attributes on element
-                     *
-                     * @param attrs
-                     */
-                    setAttrs = function (attrs) {
-                        _.forIn(attrs, function (v, k) {
-                            $element.attr(k, v);
-                        });
-                    };
-
-                $element.removeAttr('gen-attr');
-
-                if (attr) {
-                    setAttrs(attr);
-                }
-
-                $scope.$watch($attrs.genAttr, function (newVal, oldVal) {
-                    if (newVal !== oldVal) {
-                        setAttrs(newVal);
-                    }
-                });
-            }
-        };
-    }
-
-    angular.module('ui-generics').directive('genAttr', genAttr);
-})();
 /** globals Swiper,jQuery **/
 (function () {
     function genCarousel() {
@@ -1373,6 +1373,70 @@ $templateCache.put("src/components/gen-image/templates/bind-once/linked-image.ht
     }
 
     angular.module('ui-generics').directive('genCompile', genCompile);
+})();
+(function () {
+    'use strict';
+
+    function genHref() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var path = attrs.genHref,
+                    options = (attrs.hasOwnProperty('genHrefOptions') && attrs.genHrefOptions) ? scope.$eval(attrs.genHrefOptions) : null,
+                    params = (options && options.hasOwnProperty('params')) ? options.params : null,
+                    disableItemProp = attrs.hasOwnProperty('genHrefDisableItemProp');
+
+                element.removeAttr('gen-href-options');
+                element.removeAttr('gen-href-disable-item-prop');
+
+                if (!attrs.hasOwnProperty('itemprop') && !disableItemProp) {
+                    element.attr('itemprop', 'url');
+                }
+
+                /**
+                 * Setup for the link
+                 */
+                function setup() {
+                    element.unbind('click');
+                    element.removeAttr('href');
+
+                    if (path === '' || path === '#') {
+                        element.attr('href', 'javascript:void(0)');
+                    } else if (!path.indexOf('#')) { //starts with #
+                        element.bind('click', function () {
+                            var el = jQuery(path);
+
+                            jQuery('html,body').animate({
+                                scrollTop: el.offset().top - 150
+                            }, 'fast');
+                        });
+                    } else if (!path.indexOf('tel') || !path.indexOf('mail')) {
+                        element.attr('href', path);
+                    } else {
+                        element.attr('href', path);
+
+                        if (!options || !options.hasOwnProperty('preventClick') || !options.preventClick) {
+                            element.bind('click', function () {
+                                options.click(path, params);
+                                return false;
+                            });
+                        }
+                    }
+                }
+
+                setup();
+
+                attrs.$observe('genHref', function (newVal) {
+                    if (newVal !== path) {
+                        path = newVal;
+                        setup();
+                    }
+                });
+            }
+        };
+    }
+
+    angular.module('ui-generics').directive('genHref', genHref);
 })();
 (function () {
     'use strict';
@@ -1568,70 +1632,6 @@ $templateCache.put("src/components/gen-image/templates/bind-once/linked-image.ht
     angular.module('ui-generics').directive('genImage', genImage);
 })();
 
-(function () {
-    'use strict';
-
-    function genHref() {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                var path = attrs.genHref,
-                    options = (attrs.hasOwnProperty('genHrefOptions') && attrs.genHrefOptions) ? scope.$eval(attrs.genHrefOptions) : null,
-                    params = (options && options.hasOwnProperty('params')) ? options.params : null,
-                    disableItemProp = attrs.hasOwnProperty('genHrefDisableItemProp');
-
-                element.removeAttr('gen-href-options');
-                element.removeAttr('gen-href-disable-item-prop');
-
-                if (!attrs.hasOwnProperty('itemprop') && !disableItemProp) {
-                    element.attr('itemprop', 'url');
-                }
-
-                /**
-                 * Setup for the link
-                 */
-                function setup() {
-                    element.unbind('click');
-                    element.removeAttr('href');
-
-                    if (path === '' || path === '#') {
-                        element.attr('href', 'javascript:void(0)');
-                    } else if (!path.indexOf('#')) { //starts with #
-                        element.bind('click', function () {
-                            var el = jQuery(path);
-
-                            jQuery('html,body').animate({
-                                scrollTop: el.offset().top - 150
-                            }, 'fast');
-                        });
-                    } else if (!path.indexOf('tel') || !path.indexOf('mail')) {
-                        element.attr('href', path);
-                    } else {
-                        element.attr('href', path);
-
-                        if (!options || !options.hasOwnProperty('preventClick') || !options.preventClick) {
-                            element.bind('click', function () {
-                                options.click(path, params);
-                                return false;
-                            });
-                        }
-                    }
-                }
-
-                setup();
-
-                attrs.$observe('genHref', function (newVal) {
-                    if (newVal !== path) {
-                        path = newVal;
-                        setup();
-                    }
-                });
-            }
-        };
-    }
-
-    angular.module('ui-generics').directive('genHref', genHref);
-})();
 (function () {
     'use strict';
 
